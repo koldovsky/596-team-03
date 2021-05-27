ab7252bbae9c903ecbc8590800e2406ba4ef1e95
YanaLyashenko <yana.lyashenko2001@gmail.com>
2021-05-27 22:07:31 +0000

    carousel js

--- index.js/carousel.js
+++ index.js/carousel.js
@@ -1,29 +1,35 @@
 (function() {
     const items = [
-        `<div class="testimonials-icon">
-        <img src="img/tourist-review.webp" alt="tourist-review">
-    </div>
-    <article>
-        <h4 class="testimonials-username">Cole Tiers</h4>
-        <p class="testimonials-user"> I wanted to see Norway since childhood when I saw a stylish house right in the middle of the Norwegian forest in some magazine. I  thought then that I also want to live in such a house. This dream came true a few months ago on a tour of Norway. I am very happy! Thanks, guys! </p>
-        <p class="testimonials-date">October 28, 2018</p>
-    </article>`
-        `<div class="testimonials-icon">
+        ` <div class="testimonials-item">
+        <div class="testimonials-icon">
+            <img src="img/tourist-review.webp" alt="tourist-review">
+        </div>
+        <article>
+            <h4 class="testimonials-username">Cole Tiers</h4>
+            <p class="testimonials-user"> I wanted to see Norway since childhood when I saw a stylish house right in the middle of the Norwegian forest in some magazine. I  thought then that I also want to live in such a house. This dream came true a few months ago on a tour of Norway. I am very happy! Thanks, guys! </p>
+            <p class="testimonials-date">October 28, 2018</p>
+        </article>
+    </div>`,
+    `<div class="testimonials-item">
+    <div class="testimonials-icon">
         <img src="img/family-review.webp" alt="family-review">
     </div>
     <article>
         <h4 class="testimonials-username">Lilland Forester</h4>
         <p class="testimonials-user"> We went on this tour with our daughter, she was 5 years old. I was worried that it would be difficult for her but everything went great. We spent nights in good hotels with excellent breakfasts. My daughter always had something to eat, and the nature in Norway fascinated her even more than us!</p>
         <p class="testimonials-date"> May 13, 2019</p>
-    </article>`
-        `<div class="testimonials-icon">
+    </article>
+    </div>`,
+    `<div class="testimonials-item">
+    <div class="testimonials-icon">
         <img src="img/gull-review.webp" alt="gul-review">
     </div>
     <article>
         <h4 class="testimonials-username">Alice Hendricks</h4>
         <p class="testimonials-user">I still can't believe I have seen the Northern Lights with my own eyes! That was incredible! When we arrived in Tromsø, it was constantly snowing, the weather was bad, and the guide said that we were out of luck, most likely. But on the last night, right at midnight, the clouds left, and I saw it! It was magical!</p>
         <p class="testimonials-date">January 12, 2020</p>
-    </article>`
+    </article>
+    </div> `
     ];
     
     let currentIdx = 0;
@@ -45,8 +51,9 @@
         showCurrentSlide();
     }
     
-    setInterval(nextSlide, 2000);
-    
-    document.querySelector('.products-carousel .slide-prev').addEventListener('click', prevSlide);
-    document.querySelector('.products-carousel .slide-next').addEventListener('click', nextSlide);
+    // setInterval(nextSlide, 2000);
+    nextSlide()
+
+    document.querySelector('.testimonials-slider .slide-prev').addEventListener('click', prevSlide);
+    document.querySelector('.testimonials-slider .slide-next').addEventListener('click', nextSlide);
 })();
